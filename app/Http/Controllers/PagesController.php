e2404edd27fc6b331a31ee5a1de587d3cd8ff8bd
Harry <harry.pujianto@veritrans.co.id>
2015-09-29 16:12:09 +0700

    add sample notification handler

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -223,7 +223,6 @@ class PagesController extends Controller
             }   
         }
 
-        return 'charged';
     }
 
     public function transaction() 
@@ -292,6 +291,39 @@ class PagesController extends Controller
         }
 
         error_log(print_r($result,TRUE));
+
+        /*
+        $transaction = $notif->transaction_status;
+        $type = $notif->payment_type;
+        $order_id = $notif->order_id;
+        $fraud = $notif->fraud_status;
+
+        if ($transaction == 'capture') {
+          // For credit card transaction, we need to check whether transaction is challenge by FDS or not
+          if ($type == 'credit_card'){
+            if($fraud == 'challenge'){
+              // TODO set payment status in merchant's database to 'Challenge by FDS'
+              // TODO merchant should decide whether this transaction is authorized or not in MAP
+              echo "Transaction order_id: " . $order_id ." is challenged by FDS";
+              } 
+              else {
+              // TODO set payment status in merchant's database to 'Success'
+              echo "Transaction order_id: " . $order_id ." successfully captured using " . $type;
+              }
+            }
+          }
+        else if ($transaction == 'settlement'){
+          // TODO set payment status in merchant's database to 'Settlement'
+          echo "Transaction order_id: " . $order_id ." successfully transfered using " . $type;
+          } 
+          else if($transaction == 'pending'){
+          // TODO set payment status in merchant's database to 'Pending'
+          echo "Waiting customer to finish transaction order_id: " . $order_id . " using " . $type;
+          } 
+          else if ($transaction == 'deny') {
+          // TODO set payment status in merchant's database to 'Denied'
+          echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is denied.";
+        }*/
    
     }
 
