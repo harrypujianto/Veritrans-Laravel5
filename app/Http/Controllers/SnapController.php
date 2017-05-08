8161d0eaf6c336c184a054d8dc8171495f5abfc7
Harry <harry.pujianto@veritrans.co.id>
2017-05-08 15:17:47 +0700

    bug fix & add feature
    - Add expiry sample code
    - fix 3ds issue

--- app/Http/Controllers/SnapController.php
+++ app/Http/Controllers/SnapController.php
@@ -12,6 +12,7 @@ class SnapController extends Controller
     public function __construct()
     {   
         Midtrans::$serverKey = 'your server key';
+        //set is production to true for production mode
         Midtrans::$isProduction = false;
     }
 
@@ -22,38 +23,38 @@ class SnapController extends Controller
 
     public function token() 
     {
-        error_log('masuk ke snap token adri ajax');
+        error_log('masuk ke snap token dri ajax');
         $midtrans = new Midtrans;
 
         $transaction_details = array(
-            'order_id'          => uniqid(),
+            'order_id'      => uniqid(),
             'gross_amount'  => 200000
         );
 
         // Populate items
         $items = [
             array(
-                'id'                => 'item1',
-                'price'         => 100000,
+                'id'        => 'item1',
+                'price'     => 100000,
                 'quantity'  => 1,
-                'name'          => 'Adidas f50'
+                'name'      => 'Adidas f50'
             ),
             array(
-                'id'                => 'item2',
-                'price'         => 50000,
+                'id'        => 'item2',
+                'price'     => 50000,
                 'quantity'  => 2,
-                'name'          => 'Nike N90'
+                'name'      => 'Nike N90'
             )
         ];
 
         // Populate customer's billing address
         $billing_address = array(
-            'first_name'        => "Andri",
-            'last_name'         => "Setiawan",
-            'address'           => "Karet Belakang 15A, Setiabudi.",
-            'city'                  => "Jakarta",
+            'first_name'    => "Andri",
+            'last_name'     => "Setiawan",
+            'address'       => "Karet Belakang 15A, Setiabudi.",
+            'city'          => "Jakarta",
             'postal_code'   => "51161",
-            'phone'                 => "081322311801",
+            'phone'         => "081322311801",
             'country_code'  => 'IDN'
             );
 
@@ -62,28 +63,40 @@ class SnapController extends Controller
             'first_name'    => "John",
             'last_name'     => "Watson",
             'address'       => "Bakerstreet 221B.",
-            'city'              => "Jakarta",
-            'postal_code' => "51162",
-            'phone'             => "081322311801",
-            'country_code'=> 'IDN'
+            'city'          => "Jakarta",
+            'postal_code'   => "51162",
+            'phone'         => "081322311801",
+            'country_code'  => 'IDN'
             );
 
         // Populate customer's Info
         $customer_details = array(
-            'first_name'            => "Andri",
-            'last_name'             => "Setiawan",
-            'email'                     => "andrisetiawan@asdasd.com",
-            'phone'                     => "081322311801",
+            'first_name'      => "Andri",
+            'last_name'       => "Setiawan",
+            'email'           => "andrisetiawan@asdasd.com",
+            'phone'           => "081322311801",
             'billing_address' => $billing_address,
             'shipping_address'=> $shipping_address
             );
 
         // Data yang akan dikirim untuk request redirect_url.
-        // Uncomment 'credit_card_3d_secure' => true jika transaksi ingin diproses dengan 3DSecure.
+        $credit_card['secure'] = true;
+        //ser save_card true to enable oneclick or 2click
+        //$credit_card['save_card'] = true;
+
+        $time = time();
+        $custom_expiry = array(
+            'start_time' => date("Y-m-d H:i:s O",$time),
+            'unit'       => 'hour', 
+            'duration'   => 2
+        );
+        
         $transaction_data = array(
             'transaction_details'=> $transaction_details,
-            'item_details'           => $items,
-            'customer_details'   => $customer_details
+            'item_details'       => $items,
+            'customer_details'   => $customer_details,
+            'credit_card'        => $credit_card,
+            'expiry'             => $custom_expiry
         );
     
         try
